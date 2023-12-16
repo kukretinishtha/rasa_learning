11cbf719652d9d7de788c328fde341fbe623f52f
kukretinishtha <55338225+kukretinishtha@users.noreply.github.com>
2023-12-16 08:50:44 +0000

    updated flask app

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'This is rasa bot apis'
+
+@app.route("/chat")
+def index():  
+    return render_template('app.html')
+
 if __name__ == '__main__':
     app.run()
